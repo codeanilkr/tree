7310d39bf76ffe8673e8ebebb4dc99a77f5b8b82
Anil Kumar <149710011+codeanilkr@users.noreply.github.com>
2023-12-09 02:52:09 +0530

    <Binary tree>

--- TreeBasic.java
+++ TreeBasic.java
@@ -1,3 +1,40 @@
-public class treeBasic {
-    
+public class TreeBasic {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+static class Binarytree{
+    static int idx = -1;
+    public static Node buildTree(int nodes[]){
+        idx++;
+        if(nodes[idx] == -1){
+            return null;
+        }
+Node newNode = new Node(nodes[idx]);
+newNode.left = buildTree(nodes);
+newNode.left = buildTree(nodes);
+return newNode;
+
+    }
+}
+
+
+
+
+
+
+
+    public static void main(String[] args){
+        int nodes[] ={1, 2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        Binarytree tree = new Binarytree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
+
+    }
 }
